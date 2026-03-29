45534f1c4c6544f7b4642f548f7541d1e0e77d57
z4trox <kylian.lefebvre60@gmail.com>
2026-03-29 10:57:21 +0200

    Add app entry point

---
+++ Program.cs
@@ -0,0 +1,8 @@
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using Microsoft.VisualBasic.FileIO;
+
+ApplicationConfiguration.Initialize();
+Application.Run(new MainForm());
